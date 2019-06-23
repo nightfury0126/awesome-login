03470dbee447a1fc4cd8c24686e119fe24d8ef6c
BubblyBoy <aliyunus291@gmail.com>
2019-06-23 10:06:19 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,28 @@ class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        elevation: 0,
+      ),
+
+      body: Stack(
+
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5)
+            
+            ),
+            padding: EdgeInsets.only(left: 30, right: 30),
+            child: TextField(
+              textInputAction: TextInputAction.send,
+            ),
+          )
+          
+        ],
+      ),
 
     );
   }
